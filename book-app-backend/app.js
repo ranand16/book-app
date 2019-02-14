153a87dc37f842155b464d55aabd197542e0135e
ranand16 <ranand16@gmail.com>
2019-02-14 08:02:55 +0530

    Added server js and app js file.

---
+++ book-app-backend/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+const bookRoutes = require('./api/routes/books');
+const userRoutes = require('./api/routes/user');
+
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb+srv://rishabh:Rishabh1!@cluster0-cov6d.gcp.mongodb.net/test?retryWrites=true', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/books', {useNewUrlParser: true, useCreateIndex: true})
+.catch(error=>{
+    console.log("there was an error while connectinng to mongo db.");
+    console.log("error \n");
+    console.log(error);
+});
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    next();
+});
+  
+
+// all the workable routes
+app.use('/bookapi', bookRoutes);
+app.use('/userapi', userRoutes);
+
+module.exports = app;
